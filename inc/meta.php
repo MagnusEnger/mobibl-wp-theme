1291c7dac6a6dffb9c163d6b6876fca24b3885bf
Magnus Enger <magnus@enger.priv.no>
2011-09-13 21:58:37 +0200

    Try separating the signature with a hr

--- inc/meta.php
+++ inc/meta.php
@@ -1,4 +1,5 @@
 <div class="meta">
+<hr />
 	Publisert <?php the_time('Y-m-d') ?> av <?php the_author() ?>
 	<?php // comments_popup_link('No Comments', '1 Comment', '% Comments', 'comments-link', ''); ?>
 </div>
