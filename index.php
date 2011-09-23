c66f0efca33b02836d1bb9bf194156b046c4ebed
Magnus Enger <magnus@enger.priv.no>
2011-09-23 18:33:48 +0200

    Remove news/blogposts from the theme
    They will be displayed by a widget instead.

--- index.php
+++ index.php
@@ -29,13 +29,6 @@ get_header();
 
 <?php get_sidebar(); ?>
 
-    <h2>Nyheter</h2>
-    <ul id="news-menu" data-role="listview" data-inset="true" data-theme="c" data-dividertheme="b">
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <li><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></li>
-        <?php endwhile;endif ?>
-    </ul>
-
 	<?php // include (TEMPLATEPATH . '/inc/nav.php' ); ?>
 
 <?php get_footer(); ?>
